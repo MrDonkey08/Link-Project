79ba7d609123eee8363c780ca998d1298cc3ff7a
Alan <113253976+MrDonkey08@users.noreply.github.com>
2024-10-12 23:24:48 -0600

    Dist Folder

---
+++ public/dist/client/registro.js
@@ -0,0 +1,22 @@
+"use strict";
+const datos_alumno = document.getElementById("datos-alumno-div");
+const datos_asesor = document.getElementById("datos-asesor-div");
+const tipo_de_usuario = document.getElementById("tipo-de-usuario-select");
+toogle_display();
+// Event Listeners
+tipo_de_usuario.addEventListener("change", toogle_display);
+// Funciones
+function toogle_display() {
+    if (tipo_de_usuario.value === "") {
+        datos_alumno.style.display = "none";
+        datos_asesor.style.display = "none";
+    }
+    else if (tipo_de_usuario.value === "1") {
+        datos_asesor.style.display = "none";
+        datos_alumno.style.display = "block";
+    }
+    else {
+        datos_asesor.style.display = "block";
+        datos_alumno.style.display = "none";
+    }
+}
