642609b2b974bc1a1803fb98404fd11a753150c0
Alan <113253976+MrDonkey08@users.noreply.github.com>
2024-10-13 16:16:47 -0600

    Anonymous Function

--- public/src/client/registro.ts
+++ public/src/client/registro.ts
@@ -1,26 +1,32 @@
-const datos_alumno = document.getElementById("datos-alumno-div") as HTMLElement
-const datos_asesor = document.getElementById("datos-asesor-div") as HTMLElement
-const tipo_de_usuario = document.getElementById(
-	"tipo-de-usuario-select"
-) as HTMLSelectElement
+;(() => {
+	const datos_alumno = document.getElementById(
+		"datos-alumno-div"
+	) as HTMLElement
+	const datos_asesor = document.getElementById(
+		"datos-asesor-div"
+	) as HTMLElement
+	const tipo_de_usuario = document.getElementById(
+		"tipo-de-usuario-select"
+	) as HTMLSelectElement
 
-toogle_display()
+	toogle_display()
 
-// Event Listeners
+	// Event Listeners
 
-tipo_de_usuario.addEventListener("change", toogle_display)
+	tipo_de_usuario.addEventListener("change", toogle_display)
 
-// Funciones
+	// Funciones
 
-function toogle_display() {
-	if (tipo_de_usuario.value === "") {
-		datos_alumno.style.display = "none"
-		datos_asesor.style.display = "none"
-	} else if (tipo_de_usuario.value === "1") {
-		datos_asesor.style.display = "none"
-		datos_alumno.style.display = "block"
-	} else {
-		datos_asesor.style.display = "block"
-		datos_alumno.style.display = "none"
+	function toogle_display() {
+		if (tipo_de_usuario.value === "") {
+			datos_alumno.style.display = "none"
+			datos_asesor.style.display = "none"
+		} else if (tipo_de_usuario.value === "1") {
+			datos_asesor.style.display = "none"
+			datos_alumno.style.display = "block"
+		} else {
+			datos_asesor.style.display = "block"
+			datos_alumno.style.display = "none"
+		}
 	}
-}
+})()
