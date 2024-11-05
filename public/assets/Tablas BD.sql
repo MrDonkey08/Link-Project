adbdb1ffc7ce64e8e8a4631aa93dcde045862344
Juanen07 <133031128+Juanen07@users.noreply.github.com>
2024-11-05 07:52:49 -0600

    Add files via upload

---
+++ public/assets/Tablas BD.sql
@@ -0,0 +1,50 @@
+CREATE TABLE estudiante (
+    ID SERIAL PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellido_pat VARCHAR(50) NOT NULL,
+    Apellido_mat VARCHAR(50) NOT NULL,
+    Correo_institucional VARCHAR(100) UNIQUE NOT NULL,
+    Num_tel VARCHAR(10),
+    Codigo_estudiante VARCHAR(9) UNIQUE NOT NULL,
+    Carrera VARCHAR(50) NOT NULL,
+    Contraseña VARCHAR(255) NOT NULL,
+    Estado VARCHAR(20) DEFAULT 'activo',
+    Foto BYTEA,
+    Habilidades TEXT
+);
+
+
+
+CREATE TABLE asesor (
+    ID SERIAL PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellido_pat VARCHAR(50) NOT NULL,
+    Apellido_mat VARCHAR(50) NOT NULL,
+    Codigo VARCHAR(9) UNIQUE NOT NULL,
+    Correo_institucional VARCHAR(100) UNIQUE NOT NULL,
+    Departamento VARCHAR(100) NOT NULL,
+    Foto BYTEA,
+    Clave VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE proyecto (
+    ID_proyecto SERIAL PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Descripcion TEXT NOT NULL,
+    Area VARCHAR(100) NOT NULL,
+    Cupos INT CHECK (Cupos <= 3) NOT NULL,
+    Estado VARCHAR(20) DEFAULT 'activo',
+    Asesor VARCHAR(100) NOT NULL,
+    Conocimientos_requeridos TEXT,
+    Nivel_innovacion VARCHAR(20) NOT NULL,
+    Logo BYTEA
+);
+
+
+CREATE TABLE lider (
+    ID SERIAL PRIMARY KEY,
+    ID_estudiante INT REFERENCES estudiante(ID) ON DELETE CASCADE,
+    ID_proyecto INT REFERENCES proyecto(ID_proyecto) ON DELETE CASCADE
+);
+
