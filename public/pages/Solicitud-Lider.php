9e5b341898acfb7292b27d4b1f55656dff50e48a
Angel xd <98355306+Angelcanva@users.noreply.github.com>
2024-11-18 00:45:42 -0600

    listo

--- public/pages/Solicitud-Lider.php
+++ public/pages/Solicitud-Lider.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Líder de Proyecto - Solicitudes</title>
-    
+    <link rel="stylesheet" href="../assets/styles/normalize.css" />
+    <link rel="stylesheet" href="../assets/styles/Solicitud-lider.css" />
 </head>
 <body>
 
