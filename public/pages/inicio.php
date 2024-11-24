7b970abedec4017b92c7fd8a70b6b838ca6b920f
Juanen07 <133031128+Juanen07@users.noreply.github.com>
2024-11-24 03:14:07 -0600

    rascar commits

--- public/pages/inicio.php
+++ public/pages/inicio.php
@@ -1,13 +1,17 @@
 <?php
-/*$Nombre = $_SESSION['NombreUser'];
-if (!isset($_SESSION['NombreUser'])) {
-    header("Location: ../index.php");
+session_start();
+
+if (!isset($_SESSION['IDUser']) || !isset($_SESSION['NombreUser'])) {
+    header('Location: ../index.php');
     exit();
-} */
+}
+
+$nombre_usuario = $_SESSION['NombreUser'];
+
+
 require '../src/server/conecta.php';
 $con = conecta();
 
-// Consulta los proyectos con sus descripciones
 $query_proyectos = "SELECT id, nombre, cupos, descripcion FROM proyecto WHERE activo = TRUE";
 $result_proyectos = pg_query($con, $query_proyectos);
 
@@ -31,8 +35,10 @@ if ($result_proyectos) {
     <link rel="stylesheet" href="../assets/styles/inicio.css" />
   </head>
   <body>
+    
     <header>
       <h1>Link-Project</h1>
+      <p>Bienvenido, <?php echo htmlspecialchars($nombre_usuario); ?>!</p>
     </header>
     <div class="button-container">
       <a href="crear_proyecto.php" class="btn">Crear Proyecto</a>
@@ -48,7 +54,7 @@ if ($result_proyectos) {
     <div class="projects-list">
       <h2>Proyectos Disponibles</h2>
         <?php if (!empty($proyectos)): ?>
-        <?php foreach ($proyectos as $proyecto):?>
+        <?php foreach ($proyectos as $proyecto): ?>
       <div class="project-card">
         <a href="detalles_proyecto.php?id=<?php echo $proyecto['id']; ?>">
             <h3><?php echo htmlspecialchars($proyecto['nombre']); ?></h3>
@@ -66,8 +72,6 @@ if ($result_proyectos) {
             
             <p>Cupos disponibles: <?php echo htmlspecialchars($proyecto['cupos']); ?></p>
           </a>
-
-
       </div>
       <?php endforeach; ?> <?php else: ?>
       <p>No hay proyectos disponibles.</p>
